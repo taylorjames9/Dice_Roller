e0809c116a45dd4713f255ae85be46aeb8a03548
taylorjames9 <taylorjames9@gmail.com>
2016-08-10 13:08:02 -0700

    first commit for dice roll

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineController : MonoBehaviour {
+
+
+	public Transform target;
+	public float speed;
+	private bool goToTarget;
+
+	void Start(){
+		StartCoroutine ("FindTarget");
+	}
+
+	void Update() {
+		if (goToTarget) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		}
+	}
+		
+	public IEnumerator FindTarget(){
+		yield return new WaitForSeconds (3.0f);
+		goToTarget = true;
+
+		yield return 0;
+	}
+
+}
---
+++ Assets/Scripts/numberFlicker.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class numberFlicker : MonoBehaviour {
+
+
+
+	public TextMesh textField;
+	private int i =0; 
+	private int pokeDice;
+	public float al;
+
+	public GameObject diceLine_1;
+	public GameObject diceLine_2;
+
+
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log ("I got hit " + pokeDice);
+
+		if (collision.gameObject.tag.Equals ("line")) {
+			pokeDice++;
+			Debug.Log ("I got hit " + pokeDice);
+			if (pokeDice > 0) {
+				GetComponent<TextMesh> ().text = "";
+			}
+
+			if (pokeDice > 1) {
+				StartCoroutine (NumberFlickering (0.01f));
+				pokeDice = 0;
+			}
+		}
+	}
+
+	void Start(){
+		//StartCoroutine (NumberFlickering (0.01f));
+		GetComponent<TextMesh> ().text = "";
+	}
+
+
+	public IEnumerator NumberFlickering(float interval){
+		if(interval >= 0.03f){
+			interval = 0;
+			diceLine_1.SetActive (false);
+			diceLine_2.SetActive (false);
+			yield return new WaitForSeconds (0.75f);
+			yield return StartCoroutine (FadeOutNumber (1.0f));
+		} else {
+			Debug.Log ("start the flicker");
+			interval*= 1.0001f;
+			interval+= 0.001f;
+			textField.text = Random.Range (2, 12).ToString ();
+			yield return new WaitForSeconds (interval);
+			yield return StartCoroutine(NumberFlickering(interval));
+		}
+	}
+
+
+	public IEnumerator FadeOutNumber (float alpha){
+		if (GetComponent<TextMesh> ().color.a <= 0.0f) {
+			//GetComponent<TextMesh> ().gameObject.SetActive (false);
+			Debug.Log ("We're faded out");
+			yield return 0;
+		} else {
+			Debug.Log ("We're working on fading out");
+			alpha -= 0.25f;
+			al = alpha;
+			GetComponent<TextMesh> ().color = new Color (255, 255, 255, al);
+			yield return StartCoroutine (FadeOutNumber(alpha));
+		}
+
+	}
+
+}
